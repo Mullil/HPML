6382d421b6aa1bc6a9e459a54b8353345a064bef
Mullil <eetu.taatila@gmail.com>
2025-10-03 22:59:10 +0300

    Add tiling

--- src/matmul.cpp
+++ src/matmul.cpp
@@ -9,6 +9,7 @@ struct Matrix {
 };
 
 void matmul(const Matrix& A, const Matrix& B, Matrix& C) {
+    const size_t TILE = 64;
     // Check matrix dimensions
     if (A.cols != B.rows) {
         throw std::invalid_argument("Faulty input matrix dimensions");
@@ -16,11 +17,20 @@ void matmul(const Matrix& A, const Matrix& B, Matrix& C) {
     if (C.rows != A.rows || C.cols != B.cols) {
         throw std::invalid_argument("Faulty output matrix dimensions");
     }
-    // Unoptimized matrix multiplication with a flattened matrix    
-    for (size_t i = 0; i < A.rows; ++i) {
-        for (size_t j = 0; j < B.cols; ++j) {
-            for (size_t k = 0; k < A.cols; ++k) {
-                C.data[i * C.cols + j] += A.data[i * A.cols + k] * B.data[k * B.cols + j];
+    // Matmul with added tiling and contiguous memory access
+    for (size_t ii = 0; ii < A.rows; ii+=TILE) {
+        for (size_t jj = 0; jj < B.cols; jj+=TILE) {
+            for (size_t kk = 0; kk < A.cols; kk+=TILE) {
+                size_t i_max = std::min(ii + TILE, A.rows);
+                size_t j_max = std::min(jj + TILE, B.cols);
+                size_t k_max = std::min(kk + TILE, A.cols);
+                for (size_t i = ii; i < i_max; ++i) {
+                    for (size_t k = kk; k < k_max; ++k) {
+                        for (size_t j = jj; j < j_max; ++j) {
+                            C.data[i * C.cols + j] += A.data[i * A.cols + k] * B.data[k * B.cols + j];
+                        }
+                    }
+                }
             }
             
         }
